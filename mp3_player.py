bff0f7980a8045dc05d56429a267f46286c91ba6
spotts685 <spotts685@gmail.com>
2022-02-01 09:57:47 -0700

    removed class, refactored, added songs

--- mp3_player.py
+++ mp3_player.py
@@ -1,73 +1,72 @@
 import pygame
 import tkinter as tkr
-from tkinter.filedialog import askdirectory
 import os
 
-
-mp3_name = "Homegrown MP3 Player"
-dimensions = "600x600"
+music_player = tkr.Tk()
+music_player.title("Homegrown MP3 Player")
+music_player.geometry("600x600")
 font = "Helvetica 12 bold"
+
 string_var = tkr.StringVar()
-directory = askdirectory()
+directory = "songs"
 # changes current working dir to specified path
 os.chdir(directory)
 song_list = os.listdir()
+playlist = tkr.Listbox(music_player, font=font, highlightbackground="black", selectmode=tkr.SINGLE)
 
 
-class Mp3Player:
-    def __init__(self):
-        music_player = tkr.Tk()
-        music_player.title(mp3_name)
-        music_player.geometry(dimensions)
-        self.playlist = tkr.Listbox(music_player,
-                                    font=font,
-                                    highlightbackground="black",
-                                    selectmode=tkr.SINGLE)
+def build_song_list():
+    for item in song_list:
+        position = 0
+        playlist.insert(position, item)
+        position += 1
 
-        Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="Play", command=self.play,
+
+def play():
+    # active is active state, aka file selected
+    pygame.mixer.music.load(playlist.get(tkr.ACTIVE))
+    string_var.set(playlist.get(tkr.ACTIVE))
+    pygame.mixer.music.play()
+
+
+def exit_music_player():
+    pygame.mixer.music.stop()
+
+
+def pause():
+    pygame.mixer.music.pause()
+
+
+def unpause():
+    pygame.mixer.music.unpause()
+
+
+def build_widgets():
+    play_button = tkr.Button(music_player, width=5, height=3, font=font, text="Play", command=play,
                              highlightbackground="red",
                              fg="white")
-        Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="Stop",
-                             command=self.exit_music_player,
+    stop_button = tkr.Button(music_player, width=5, height=3, font=font, text="Stop", command=exit_music_player,
                              highlightbackground="purple", fg="white")
-        Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="Pause", command=self.pause,
-                             highlightbackground="green", fg="white")
-        Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="Unpause", command=self.unpause,
-                             highlightbackground="blue", fg="white")
+    pause_button = tkr.Button(music_player, width=5, height=3, font=font, text="Pause", command=pause,
+                              highlightbackground="green", fg="white")
+    unpause_button = tkr.Button(music_player, width=5, height=3, font=font, text="Unpause", command=unpause,
+                                highlightbackground="blue", fg="white")
 
-        song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=string_var)
+    song_title = tkr.Label(music_player, font=font, textvariable=string_var)
 
-        # arrange the widgets
-        song_title.pack()
-        # makes all the widgets the same size as the container
-        Button1.pack(fill="x")
-        Button2.pack(fill="x")
-        Button3.pack(fill="x")
-        Button4.pack(fill="x")
-        self.playlist.pack(fill="both", expand="yes")
+    # arrange the widgets
+    song_title.pack()
+    # makes all the widgets the same size as the container
+    play_button.pack(fill="x")
+    stop_button.pack(fill="x")
+    pause_button.pack(fill="x")
+    unpause_button.pack(fill="x")
+    playlist.pack(fill="both", expand="yes")
 
-        music_player.mainloop()
 
-    def build_song_list(self):
-        for item in song_list:
-            position = 0
-            self.playlist.insert(position, item)
-            position += 1
+build_song_list()
+pygame.init()
+pygame.mixer.init()
 
-    def play(self):
-        # active is active state, aka file selected
-        pygame.mixer.music.load(self.playlist.get(tkr.ACTIVE))
-        string_var.set(self.playlist.get(tkr.ACTIVE))
-        pygame.mixer.music.play()
-
-    @staticmethod
-    def exit_music_player():
-        pygame.mixer.music.stop()
-
-    @staticmethod
-    def pause():
-        pygame.mixer.music.pause()
-
-    @staticmethod
-    def unpause():
-        pygame.mixer.music.unpause()
+build_widgets()
+music_player.mainloop()
